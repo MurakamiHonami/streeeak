a32bf45dc36783f8dad0865fb5d665be04d1a284
tama <mnv2525@gmail.com>
2026-03-15 17:44:06 +0900

    fix auth error

--- infrastructure/lambda/cognito_message/main.py
+++ infrastructure/lambda/cognito_message/main.py
@@ -1,15 +1,19 @@
 def lambda_handler(event, context):
-    if event['triggerSource'] == "CustomMessage_SignUp":
-        
-        username = event['userName']
-        code_placeholder = event['request']['codeParameter']
+    trigger = event.get("triggerSource", "")
+    username = event.get("userName", "")
+    code_placeholder = event.get("request", {}).get("codeParameter", "{####}")
+
+    # Helpful for confirming invocation in CloudWatch logs.
+    print(f"custom-message trigger={trigger} user={username}")
+
+    if trigger in {"CustomMessage_SignUp", "CustomMessage_ResendCode"}:
         verify_url = f"https://streeeak.link/verify?username={username}&code={code_placeholder}"
-        
-        event['response']['emailSubject'] = "【Streeeak】メール認証のお願い"
-        event['response']['emailMessage'] = f"""
-        Streeeakへようこそ！<br><br>
-        以下のリンクをクリックして、アカウントの認証を完了してください。<br>
-        <a href='{verify_url}'>こちらをクリックして認証を完了する</a>
-        """
+        event["response"]["emailSubject"] = "Streeeak - Verify your email"
+        event["response"]["emailMessage"] = (
+            "<p>Welcome to Streeeak.</p>"
+            "<p>Please verify your email from the link below:</p>"
+            f"<p><a href='{verify_url}'>Verify email</a></p>"
+            f"<p>If the link does not work, enter this code: <b>{code_placeholder}</b></p>"
+        )
 
     return event
